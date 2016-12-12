519629d26e29e66509eb9d31d30e3026de48fa1b
Daniel Rich <drich@convirza.com>
2016-12-12 14:25:41 -0700

    Getting a bunch of stuff working and pulled in.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('requirements.txt', encoding='utf-8') as reqs:
+    install_requires = [l for l in reqs.read().split('\n')]
+
 setup(name='falcon-jwt',
       version='1.0',
       description="Basic jwt support for falcon",
       url='https://github.com/trendiguru/falcon-jwt',
       py_modules=['falcon_jwt'],
+      install_requires=install_requires,
       )
