097375677a80a1a16bd501371cdfeb864427ddcb
Daniel Rich <drich@convirza.com>
2016-12-12 14:15:05 -0700

    Just getting a small simple setup.py file to make things easier.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='falcon-jwt',
+      version='1.0',
+      description="Basic jwt support for falcon",
+      url='https://github.com/trendiguru/falcon-jwt',
+      py_modules=['falcon_jwt'],
+      )
