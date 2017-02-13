062eb6835623b91ee19d729f32beec1676d19a39
Daniel Rich <qianyilong@gmail.com>
2017-02-13 10:22:48 -0700

    Allow the token to be stored and used as either a cookie or a header
    * Starting work on being able to store the tokens in headers.
    
    Currently it is assumed that things get stored as cookies but just changing it
    so that you can optionally choose to use it as a cookie or as a header. It did
    require a little renaming so that it was clear that the token was not neccesarily
    a cookie.
    
    * Getting stuff handled for using the token in a header.
    
    * Getting pylint working on the project.
    
    * Default to using the token as a cookie.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 with open('requirements.txt', encoding='utf-8') as reqs:
-    install_requires = [l for l in reqs.read().split('\n')]
+    INSTALL_REQUIRES = [l for l in reqs.read().split('\n')]
 
 setup(name='falcon-jwt',
       version='1.0',
       description="Basic jwt support for falcon",
       url='https://github.com/trendiguru/falcon-jwt',
       py_modules=['falcon_jwt'],
-      install_requires=install_requires,
-      )
+      install_requires=INSTALL_REQUIRES,
+     )
